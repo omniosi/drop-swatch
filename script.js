0f8620ced83225b2ef036a9fb903eed115317d81
Ornette Coleman <omniosi@gmail.com>
2019-05-05 18:58:19 -0700

    ADD: type check limitation

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ function onDragStart(dragEvent) {
 
 function onDragEnter(e) {
   console.log('onDragEnter');
-  e.preventDefault();
-  e.target.style.opacity = 0.5;
+  if ([...e.dataTransfer.types].includes('text/plain')) {
+    e.target.style.opacity = 0.5;
+  }
 }
 
 function onDragLeave(e) {
   console.log('onDragLeave');
-  e.preventDefault();
   e.target.style.opacity = 1;
 }
 
 function onDrop(dropEvent) {
-  dropEvent.preventDefault();
   console.log("dropEvent");
-
-  console.log('This fill is ', dropEvent.dataTransfer.getData('text/plain'));
-
+  if ([...dropEvent.dataTransfer.types].includes('text/plain')) {
+    dropEvent.preventDefault();
+    console.log('This fill is ', dropEvent.dataTransfer.getData('text/plain'));
+  }
 }
