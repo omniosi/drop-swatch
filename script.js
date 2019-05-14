2efde1053d9008b7cf2a19ed5c856f26c1383e2a
Ornette Coleman <omniosi@gmail.com>
2019-05-13 22:44:30 -0700

    FIX: data passed.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function dragStart(e) {
   console.log("dragStart");
-  // Add the target element's svg fill to the data transfer object
-  e.dataTransfer.setData('text', e.target.dataset.swatch);
-  e.dataTransfer.effectAllowed = 'copy';
-  console.log('data set = ', e.dataTransfer.getData('text'));
+  if (e.target instanceof HTMLLIElement) {
+    e.dataTransfer.setData('text', e.target.dataset.swatch);
+    e.dataTransfer.effectAllowed = 'copy';
+    console.log('data set = ', e.dataTransfer.getData('text'));
+  } else {
+    e.preventDefault();
+  }
 }
 
 function dragEnter(e) {
   console.log('onDragEnter');
-  if ([...e.dataTransfer.types].includes('text/plain')) {
+  if ([...e.dataTransfer.types].includes('text')) {
     e.preventDefault();
     e.target.style.opacity = 0.5;
   }
@@ -16,10 +19,8 @@ function dragEnter(e) {
 
 function dragOver(e) {
   console.log('onDragOver');
-  if ([...e.dataTransfer.types].includes('text')) {
-    e.preventDefault();
-  }
-
+  e.dataTransfer.dropEffect = 'copy';
+  e.preventDefault();
 }
 
 function dragLeave(e) {
@@ -30,12 +31,12 @@ function dragLeave(e) {
 
 function onDrop(e) {
   console.log("onDrop");
-  if ([...e.dataTransfer.types].includes('text')) {
-    e.preventDefault();
-    const dropTarget = e.currentTarget;
-    dropTarget.fill = e.dataTransfer.getData('text');
-    // e.target.style.opacity = 1;
-    console.log('This fill is ', e.dataTransfer.getData('text'));
-  }
+  // if ([...e.dataTransfer.types].includes('text')) {
+  e.preventDefault();
+  const dropTarget = e.target;
+  dropTarget.fill = e.dataTransfer.getData('text');
+  e.target.style.opacity = 1;
+  console.log('This fill is ', e.dataTransfer.getData('text'));
+  // }
   // return false;
 }
