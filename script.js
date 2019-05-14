517d5ea74cdf86bf1c257dd0ee303637ec4ad149
Ornette Coleman <omniosi@gmail.com>
2019-05-13 21:13:29 -0700

    stopping point

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ function onDragStart(dragEvent) {
   // Add the target element's svg fill to the data transfer object
   const dragged = dragEvent.target;
   const svg = dragged.getElementsByTagName('svg')[0];
+  // dragEvent.dataTransfer.effectAllowed = 'copy';
   dragEvent.dataTransfer.setData("text/plain", svg.fill);
+  console.log('data set = ', dragEvent.dataTransfer.getData("text/plain"));
 }
 
 function onDragEnter(e) {
@@ -24,16 +26,18 @@ function onDragOver(e) {
 
 function onDragLeave(e) {
   console.log('onDragLeave');
+  e.preventDefault();
   e.target.style.opacity = 1;
 }
 
 function onDrop(e) {
   console.log("onDrop");
   if ([...e.dataTransfer.types].includes('text/plain')) {
-    console.log('This fill is ', e.dataTransfer.getData('text/plain'));
     e.preventDefault();
-    const dropTarget = e.target;
+    const dropTarget = e.currentTarget;
     dropTarget.fill = e.dataTransfer.getData('text/plain');
     e.target.style.opacity = 1;
+    console.log('This fill is ', e.dataTransfer.getData('text/plain'));
   }
+  return false;
 }
