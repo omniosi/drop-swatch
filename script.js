5f0091aeb7aa38c10210b5e5ac94b061741d9a32
Ornette Coleman <omniosi@gmail.com>
2019-05-15 19:52:36 -0700

    FIX: drop working.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function dragStart(e) {
-  console.log("dragStart");
+  // console.log("dragStart");
   if (e.target instanceof HTMLLIElement) {
     e.dataTransfer.setData('text', e.target.dataset.swatch);
     e.dataTransfer.effectAllowed = 'copy';
-    console.log('data set = ', e.dataTransfer.getData('text'));
+    // console.log('data set = ', e.dataTransfer.getData('text'));
   } else {
     e.preventDefault();
   }
@@ -18,25 +18,22 @@ function dragEnter(e) {
 }
 
 function dragOver(e) {
-  console.log('onDragOver');
+  // console.log('onDragOver');
   e.dataTransfer.dropEffect = 'copy';
   e.preventDefault();
 }
 
 function dragLeave(e) {
   console.log('onDragLeave');
-  e.preventDefault();
+  // e.preventDefault();
   e.target.style.opacity = 1;
 }
 
 function onDrop(e) {
-  console.log("onDrop");
-  // if ([...e.dataTransfer.types].includes('text')) {
+  // console.log("onDrop");
   e.preventDefault();
   const dropTarget = e.target;
-  dropTarget.fill = e.dataTransfer.getData('text');
-  e.target.style.opacity = 1;
-  console.log('This fill is ', e.dataTransfer.getData('text'));
-  // }
-  // return false;
+  dropTarget.style.fill = e.dataTransfer.getData('text');
+  // e.target.style.opacity = 1;
+  // console.log('This fill is ', e.dataTransfer.getData('text'));
 }
